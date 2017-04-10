7c387b69b5e73c1cb7e9188f942e29ce83afb3fd
nuttycat <nuttycat@126.com>
2017-04-10 19:06:06 +0800

    init commit for zk test code

---
+++ src/main/java/kimed/me/zktest/Executor.java
@@ -0,0 +1,129 @@
+package kimed.me.zktest;
+
+/**
+ * A simple example program to use DataMonitor to start and
+ * stop executables based on a znode. The program watches the
+ * specified znode and saves the data that corresponds to the
+ * znode in the filesystem. It also starts the specified program
+ * with the specified arguments when the znode exists and kills
+ * the program if the znode goes away.
+ */
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+public class Executor
+    implements Watcher, Runnable, DataMonitor.DataMonitorListener
+{
+    String znode;
+
+    DataMonitor dm;
+
+    ZooKeeper zk;
+
+    String filename;
+
+    String exec[];
+
+    Process child;
+
+    public Executor(String hostPort, String znode, String filename,
+            String exec[]) throws KeeperException, IOException {
+        this.filename = filename;
+        this.exec = exec;
+        System.out.println("1");
+        zk = new ZooKeeper(hostPort, 3000, this);
+        System.out.println("2");
+        dm = new DataMonitor(zk, znode, null, this);
+        System.out.println("3");
+        
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String hostPort = "192.168.0.230:2181,192.168.0.231:2181,192.168.0.232:2181";
+        String znode = "/testzk";
+        String filename = "test.txt";
+        String exec[] = {"",""};
+        try {
+            new Executor(hostPort, znode, filename, exec).run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /***************************************************************************
+     * We do process any events ourselves, we just need to forward them on.
+     *
+     * @see org.apache.zookeeper.Watcher#process(org.apache.zookeeper.proto.WatcherEvent)
+     */
+    public void process(WatchedEvent event) {
+    	System.out.println("excutor wather process");
+        dm.process(event);
+    }
+
+    public void run() {
+        try {
+            synchronized (this) {
+                while (!dm.dead) {
+                    wait();
+                }
+            }
+        } catch (InterruptedException e) {
+        }
+    }
+
+    public void closing(int rc) {
+        synchronized (this) {
+            notifyAll();
+        }
+    }
+
+    static class StreamWriter extends Thread {
+        OutputStream os;
+
+        InputStream is;
+
+        StreamWriter(InputStream is, OutputStream os) {
+            this.is = is;
+            this.os = os;
+            start();
+        }
+
+        public void run() {
+            byte b[] = new byte[80];
+            int rc;
+            try {
+                while ((rc = is.read(b)) > 0) {
+                    os.write(b, 0, rc);
+                }
+            } catch (IOException e) {
+            }
+
+        }
+    }
+
+    public void exists(byte[] data) {
+        if (data == null) {
+            System.out.println("node no data");
+        } else {
+        	System.out.println("node data:" + data);
+            try {
+                FileOutputStream fos = new FileOutputStream(filename);
+                fos.write(data);
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
---
+++ src/main/java/kimed/me/zktest/ZkBarrier.java
@@ -0,0 +1,141 @@
+package kimed.me.zktest;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.data.Stat;
+
+public class ZkBarrier {
+	static final int size = 100;
+	static final String root = "/barrier";
+	static ZooKeeper zk = null;
+	Integer mutex;
+	String nodeName;
+
+	ZkBarrier(String address, String nodeName) {
+		this.nodeName = nodeName;
+		mutex = new Integer(-1);
+		if (zk == null) {
+			try {
+				zk = new ZooKeeper(address, 3000, new ZkWatcher());
+				if (zk != null) {
+					Stat s = zk.exists(root, true);
+					if (s == null) {
+						zk.create(root, new byte[0], Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (KeeperException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private class ZkWatcher implements Watcher {
+		public void process(WatchedEvent event) {
+			System.out.println(new Date().getTime() + ":get ZkWatcher watch even,path:" + event.getPath() + ",type:"
+					+ event.getType() + ",state:" + event.getState());
+		}
+	}
+
+	private class BarrierWatcher implements Watcher {
+		public void process(WatchedEvent event) {
+			System.out.println(new Date().getTime() + ":get BarrierWatcher watch even,path:" + event.getPath()
+					+ ",type:" + event.getType() + ",state:" + event.getState());
+			synchronized (mutex) {
+				mutex.notify();
+			}
+		}
+	}
+
+	private boolean enter() {
+		try {
+			zk.create(root + "/" + nodeName, new byte[0], Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+			BarrierWatcher bw = new BarrierWatcher();
+			while (true) {
+				synchronized (mutex) {
+					List<String> childrenList = zk.getChildren(root, bw);
+					if (childrenList.size() < size) {
+						mutex.wait();
+					} else {
+						return true;
+					}
+				}
+			}
+		} catch (KeeperException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	private boolean leave() {
+		try {
+			zk.delete(root + "/" + nodeName, 0);
+
+			synchronized (mutex) {
+				while (true) {
+					List<String> childrenList = zk.getChildren(root, new BarrierWatcher());
+					if (childrenList.size() > 0) {
+						mutex.wait();
+					} else {
+						return true;
+					}
+				}
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (KeeperException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	class ZkRunable implements Runnable {
+		private ZkBarrier zkBarrier;
+
+		ZkRunable(ZkBarrier zkBarrier) {
+			this.zkBarrier = zkBarrier;
+		}
+
+		public void run() {
+			if (zkBarrier.enter()) {
+				System.out.println("node:" + nodeName + " get notify:all entered.");
+				try {
+					Thread.sleep(2 * 2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				if (zkBarrier.leave()) {
+					System.out.println("node:" + nodeName + " get notify:all leaved.");
+				}
+			}
+			try {
+				Thread.sleep(100000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		String hostAddress = "192.168.0.230:2181,192.168.0.231:2181,192.168.0.232:2181";
+		int size = ZkBarrier.size;
+		for (int i = 0; i < size; i++) {
+			ZkBarrier zkBr = new ZkBarrier(hostAddress, "b" + i);
+			Thread t = new Thread(zkBr.new ZkRunable(zkBr));
+			t.start();
+		}
+	}
+}
